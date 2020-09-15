541e823cba9d759d71b31964b2a66786a4bf950b
derGoldeneJonas <jonas@bender-home.eu>
2020-09-16 01:38:21 +0200

    swap server adress

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://tedt:test1234@cluster0.0pgbs.mongodb.net/test?retryWrites=true",
+    mongoURI: "mongodb://canu-client:Cr34t1v1ty@creativity.lfe.mw.tum.de:27017/creativityDB",
     secretOrKey: "secret"
 };
